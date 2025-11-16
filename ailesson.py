a2b72d6aaf4d972cf92041e032cf52bd9e055de6
rishaan2025 <joyeeta.mukherjee@gmail.com>
2025-11-16 09:33:49 -0800

    Update ailesson.py

--- ailesson.py
+++ ailesson.py
@@ -1,1 +1,26 @@
-hi
+import colorama
+from colorama import Fore, Style
+from textblob import TextBlob
+colorama.init()
+print(f"{Fore.CYAN}🎉👏🚀 Welcome to Sentiment Spy! 🕵️ {Style.RESET_ALL} ")
+user_name=(f"{Fore.MAGENTA}Please enter your name: {Style.RESET_ALL}")
+if not user_name:
+    user_name="Mystery Agent"
+conversation_history=[]
+print(f"\n{Fore.CYAN}Hello Agent {user_name}!")
+print(f"Type a sentence and I will analyze your sentences with TextBlob and show you the sentiment. 🔍")
+print(f"Type {Fore.YELLOW}'reset'{Fore.CYAN},{Fore.YELLOW}'history'{Fore,CYAN},"f"or{Fore.YELLOW}'exit'{Fore.CYAN} to quit.{Style.RESET_ALL}\n")
+while True: 
+    user_input=input(f"{Fore.GREEN}>>{Style.RESET_ALL}").strip()
+    if not user_input:
+        print(f"{Fore.RED}Please enter some text or a valid command.{Style.RESET_ALL}")
+        continue
+    if user_input.lower()=="exit":
+        print(f"\n{Fore.BLUE}😭Exiting Sentiment Spy. Farewell, Agent{user_name}! 🏁{style.RESET_ALL}")
+        break
+    elif user_input.lower()=='reset':
+        if not conversation_history:
+            print(f"{Fore.YELLOW}No conversation history yet.{Style.RESET_ALL}")
+        else:
+            print(f"{Fore.CYAN}📜 Conversation History:{Style.RESET_ALL}")
+            for idx, (text,polarity,sentiment_type) in enumerate(conversation_history,start=1)
